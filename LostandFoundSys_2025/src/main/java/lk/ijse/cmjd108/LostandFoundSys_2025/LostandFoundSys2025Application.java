c1912a4250b5f9cb1ce1afc1d75da042f1bf7638
Ashan Randima Manamperi <120651088+Dettlaff404@users.noreply.github.com>
2025-05-07 14:37:37 +0530

    Project Initialization

---
+++ LostandFoundSys_2025/src/main/java/lk/ijse/cmjd108/LostandFoundSys_2025/LostandFoundSys2025Application.java
@@ -0,0 +1,13 @@
+package lk.ijse.cmjd108.LostandFoundSys_2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LostandFoundSys2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LostandFoundSys2025Application.class, args);
+	}
+
+}
---
+++ LostandFoundSys_2025/src/test/java/lk/ijse/cmjd108/LostandFoundSys_2025/LostandFoundSys2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.cmjd108.LostandFoundSys_2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LostandFoundSys2025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
