a1a11e744e4a9bd63cd465b8efb328f6cf7b8cbc
Ashan Randima Manamperi <120651088+Dettlaff404@users.noreply.github.com>
2025-05-07 17:18:31 +0530

    Added ModelMapper and Updated dependencies

--- LostandFoundSys_2025/src/main/java/lk/ijse/cmjd108/LostandFoundSys_2025/LostandFoundSys2025Application.java
+++ LostandFoundSys_2025/src/main/java/lk/ijse/cmjd108/LostandFoundSys_2025/LostandFoundSys2025Application.java
@@ -1,10 +1,21 @@
 package lk.ijse.cmjd108.LostandFoundSys_2025;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableJpaRepositories
+@EnableWebMvc
 public class LostandFoundSys2025Application {
+
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(LostandFoundSys2025Application.class, args);
