013f95e8931f4ce0af7775533e2e0a81d7a5b8e1
Ashan Randima Manamperi <120651088+Dettlaff404@users.noreply.github.com>
2025-05-08 20:10:55 +0530

    added logger

--- LostandFoundSys_2025/src/main/java/lk/ijse/cmjd108/LostandFoundSys_2025/LostandFoundSys2025Application.java
+++ LostandFoundSys_2025/src/main/java/lk/ijse/cmjd108/LostandFoundSys_2025/LostandFoundSys2025Application.java
@@ -1,6 +1,8 @@
 package lk.ijse.cmjd108.LostandFoundSys_2025;
 
 import org.modelmapper.ModelMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -12,12 +14,15 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @EnableWebMvc
 public class LostandFoundSys2025Application {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(LostandFoundSys2025Application.class);
+
 	@Bean
 	public ModelMapper modelMapper() {
 		return new ModelMapper();
 	}
 
 	public static void main(String[] args) {
+		LOGGER.info("Application started");
 		SpringApplication.run(LostandFoundSys2025Application.class, args);
 	}
 
