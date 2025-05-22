524f2891ea332a17d4f0464d1212145114af5303
Ashan Randima Manamperi <120651088+Dettlaff404@users.noreply.github.com>
2025-05-22 13:00:25 +0530

    Updated UserDao

--- LostandFoundSys_2025/src/main/java/lk/ijse/cmjd108/LostandFoundSys_2025/dao/UserDao.java
+++ LostandFoundSys_2025/src/main/java/lk/ijse/cmjd108/LostandFoundSys_2025/dao/UserDao.java
@@ -1,5 +1,7 @@
 package lk.ijse.cmjd108.LostandFoundSys_2025.dao;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,5 @@ import lk.ijse.cmjd108.LostandFoundSys_2025.entities.UserEntity;
 
 @Repository
 public interface UserDao extends JpaRepository<UserEntity, String> {
-
+    Optional<UserEntity> findByEmail(String email);
 }
