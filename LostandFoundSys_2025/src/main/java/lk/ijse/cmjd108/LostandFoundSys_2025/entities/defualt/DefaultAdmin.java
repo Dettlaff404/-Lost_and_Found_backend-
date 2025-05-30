a35a09c1724e58a816312400e6844afff39aef67
Ashan Randima Manamperi <120651088+Dettlaff404@users.noreply.github.com>
2025-05-30 10:21:13 +0530

    Enter default admin properties with value injections from properties file

--- LostandFoundSys_2025/src/main/java/lk/ijse/cmjd108/LostandFoundSys_2025/entities/defualt/DefaultAdmin.java
+++ LostandFoundSys_2025/src/main/java/lk/ijse/cmjd108/LostandFoundSys_2025/entities/defualt/DefaultAdmin.java
@@ -2,6 +2,7 @@ package lk.ijse.cmjd108.LostandFoundSys_2025.entities.defualt;
 
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Value;
 
 import jakarta.annotation.PostConstruct;
 import lk.ijse.cmjd108.LostandFoundSys_2025.dao.UserDao;
@@ -11,6 +12,7 @@ import lk.ijse.cmjd108.LostandFoundSys_2025.util.EntityDTO_Convertor;
 import lk.ijse.cmjd108.LostandFoundSys_2025.util.UtilData;
 import lombok.RequiredArgsConstructor;
 
+
 @Component
 @RequiredArgsConstructor
 public class DefaultAdmin {
@@ -19,14 +21,32 @@ public class DefaultAdmin {
     private final EntityDTO_Convertor entityDTOConvertor;
     private final PasswordEncoder passwordEncoder;
 
+    // value injections for defualt admin user
+    @Value("${fullName}")
+    private String fullName;
+
+    @Value("${email}")
+    private String email;
+
+    @Value("${mobile}")
+    private String mobile;
+
+    @Value("${password}")
+    private String password;
+
+    /**
+     * This method is called after the bean is created and initializes a default admin user
+     * if it does not already exist in the database.
+     */
+
     @PostConstruct
     public void addDefaultAdminUser() {
         UserDTO adminUser = new UserDTO();
         adminUser.setUserId(UtilData.generateUserId());
-        adminUser.setFullname("Admin");
-        adminUser.setEmail("admin@mail.com");
-        adminUser.setMobile("0123456789");
-        adminUser.setPassword(passwordEncoder.encode("Admin2025@Lost&found"));
+        adminUser.setFullname(fullName);
+        adminUser.setEmail(email);
+        adminUser.setMobile(mobile);
+        adminUser.setPassword(passwordEncoder.encode(password));
         adminUser.setRole(Role.ADMIN);
 
         if (!(userDao.findByEmail(adminUser.getEmail()).isPresent())) {
